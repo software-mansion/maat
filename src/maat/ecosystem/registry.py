1d2cfcc51abb46cbe154a62c788f591167a6a206
Marek Kaput <marek.kaput@swmansion.com>
2025-04-01 15:01:15 +0200

    implement downloading packages from registries

--- src/maat/ecosystem/registry.py
+++ src/maat/ecosystem/registry.py
@@ -1,5 +1,107 @@
+import re
+from functools import lru_cache
+from typing import Self
+from urllib.parse import urljoin
+
+import requests
+from pydantic import BaseModel, RootModel
+
+from maat.ecosystem import scarbs_xyz
 from maat.model import Step
+from maat.utils.unique_id import unique_id
 
 
 def setup_registry(registry_url: str, package: str) -> list[Step]:
-    raise NotImplementedError
+    registry_config = RegistryConfig.fetch(registry_url)
+    records = IndexRecords.fetch(registry_config, package)
+
+    latest = records.latest_version()
+    dl_url = registry_config.expand_dl(package, latest)
+
+    # Using unique IDs to reduce risks of potential conflicts with archive contents.
+    dl_path = f"archive-{unique_id()}.tar.zstd"
+
+    return [
+        Step.setup(["curl", "-sSLf", dl_url, "-o", dl_path]),
+        Step.setup(["tar", "--zstd", "-xf", dl_path, "--strip-components", "1"]),
+    ]
+
+
+class RegistryConfig(BaseModel):
+    dl: str
+    index: str
+
+    @classmethod
+    @lru_cache
+    def fetch(cls, registry_url: str) -> Self:
+        url = cls.config_json_url(registry_url)
+        return cls.model_validate_json(requests.get(url).content)
+
+    @classmethod
+    def config_json_url(cls, registry_url: str) -> str:
+        if registry_url == scarbs_xyz.BASE_URL:
+            # Workaround for https://github.com/software-mansion/scarbs.xyz/issues/262
+            return urljoin(registry_url, "/api/v1/index/config.json")
+        else:
+            return urljoin(registry_url, "/config.json")
+
+    def expand_dl(self, package: str, version: str) -> str:
+        return self.dl.replace("{package}", package).replace("{version}", version)
+
+    def expand_index(self, package: str) -> str:
+        return self.index.replace("{prefix}", _package_prefix(package)).replace(
+            "{package}", package
+        )
+
+
+class IndexRecord(BaseModel):
+    v: str
+
+
+class IndexRecords(RootModel):
+    root: list[IndexRecord]
+
+    @classmethod
+    def fetch(cls, registry: RegistryConfig, package: str) -> Self:
+        url = registry.expand_index(package)
+        return cls.model_validate_json(requests.get(url).content)
+
+    def latest_version(self) -> str:
+        latest_record = max(self.root, key=lambda record: _split_semver(record.v))
+        return latest_record.v
+
+
+def _package_prefix(name: str) -> str:
+    """Make a path to a package directory, which aligns to the index directory layout."""
+    match len(name):
+        case 1:
+            return "1"
+        case 2:
+            return "2"
+        case 3:
+            return f"3/{name[:1]}"
+        case _:
+            return f"{name[:2]}/{name[2:4]}"
+
+
+def _split_semver(semver: str) -> list[int | str]:
+    """
+    Split a semantic version into its components.
+
+    ```
+    1.2.3 -> [1, 2, 3]
+    0.1.0-alpha -> [0, 1, 0, "alpha"]
+    0.1.0-alpha.1 -> [0, 1, 0, "alpha", 1]
+    0.1.0-alpha.1+2024 -> [0, 1, 0, "alpha", 1, 2024]
+    ```
+    """
+
+    components = re.split(r"[.\-+]", semver)
+    return [try_int(c) for c in components]
+
+
+def try_int(c: str) -> int | str:
+    if c.isdigit():
+        return int(c)
+    else:
+        return c
