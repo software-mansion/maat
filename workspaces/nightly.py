33571a63e5b195c0942e19600c40309492bc1afc
Marek Kaput <marek.kaput@swmansion.com>
2025-05-13 15:42:29 +0200

    limit what is accessible when generating report names

--- workspaces/nightly.py
+++ workspaces/nightly.py
@@ -1,4 +1,4 @@
-from maat.ecosystem.spec import import_workspace
+from maat.ecosystem.spec import import_workspace, ReportNameGenerationContext
 
 release = import_workspace("release")
 
@@ -8,5 +8,5 @@ default_scarb = "latest:nightly"
 default_foundry = "latest:nightly"
 
 
-def generate_report_name(report):
-    return f"{report.workspace}-latest"
+def generate_report_name(ctx: ReportNameGenerationContext):
+    return f"{ctx.workspace}-latest"
