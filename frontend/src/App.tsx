22473b5493123cf8dbd337994dc1b1bf177e8201
Marek Kaput <marek.kaput@swmansion.com>
2025-07-25 13:35:50 +0200

    add error boundary

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import { Footer } from "./Footer.tsx";
-import { Navbar } from "./Navbar.tsx";
 import { Suspense } from "react";
 import { viewModelAtom } from "./atoms.ts";
 import { useAtomValue } from "jotai";
+import { ErrorBoundary, type FallbackProps } from "react-error-boundary";
 
 export function App() {
   return (
     <>
-      <Navbar />
-      <Suspense fallback={<Loading />}>
-        <PreviewViewModel />
-      </Suspense>
+      <header className="flex flex-col items-center justify-center px-12 py-8">
+        <h1 className="font-fancy text-5xl">Ma'at</h1>
+      </header>
+      <ErrorBoundary FallbackComponent={Fallback}>
+        <Suspense fallback={<Loading />}>
+          <PreviewViewModel />
+        </Suspense>
+      </ErrorBoundary>
       <Footer />
     </>
   );
@@ -20,6 +24,14 @@ function Loading() {
   return (
     <div className="m-10 flex justify-center">
       <div className="loading loading-infinity text-secondary loading-xl"></div>
+    </div>
+  );
+}
+
+function Fallback({ error }: FallbackProps) {
+  return (
+    <div role="alert" className="alert alert-error m-8">
+      <pre>{error.message}</pre>
     </div>
   );
 }
