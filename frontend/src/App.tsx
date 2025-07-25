7ba36467c1aca2cf76922d4fff6ab2f078d4a612
Marek Kaput <marek.kaput@swmansion.com>
2025-07-25 16:20:12 +0200

    working slicer and downloads

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { Footer } from "./Footer.tsx";
 import { Suspense } from "react";
-import { viewModelAtom } from "./atoms.ts";
-import { useAtomValue } from "jotai";
 import { ErrorBoundary, type FallbackProps } from "react-error-boundary";
+import { Slicer } from "./Slicer.tsx";
+import { DownloadsSection } from "./Downloads.tsx";
 
 export function App() {
   return (
@@ -13,26 +13,8 @@ export function App() {
       <ErrorBoundary FallbackComponent={Fallback}>
         <Suspense fallback={<Loading />}>
           <main className="flex flex-col gap-4 px-4">
-            <PreviewViewModel />
-            <div className="collapse-arrow border-base-300 bg-base-100 collapse overflow-hidden border">
-              <input type="checkbox" />
-              <div className="collapse-title font-bold select-none">Downloads</div>
-              <table className="collapse-content table-pin-rows table-pin-cols table">
-                <thead>
-                  <tr>
-                    <th>Data exports</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th>Ecosystem (CSV)</th>
-                  </tr>
-                  <tr>
-                    <th>Ecosystem (JSON)</th>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <Slicer />
+            <DownloadsSection />
           </main>
         </Suspense>
       </ErrorBoundary>
@@ -55,9 +37,4 @@ function Fallback({ error }: FallbackProps) {
       <pre>{error.message}</pre>
     </div>
   );
-}
-
-function PreviewViewModel() {
-  const viewModel = useAtomValue(viewModelAtom);
-  return <pre>{`${JSON.stringify(viewModel, null, 2)}`}</pre>;
 }
