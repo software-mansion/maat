2563d169ef4051f74c17f3c8ca3f411607d86055
Marek Kaput <marek.kaput@swmansion.com>
2025-08-04 12:50:13 +0200

    better Q element

--- frontend/src/Q.tsx
+++ frontend/src/Q.tsx
@@ -1,9 +1,11 @@
 import type { ReactNode } from "react";
+import { VscInfo } from "react-icons/vsc";
 
 export function Q({ children }: { children: ReactNode }) {
   return (
-    <span className="tooltip badge badge-xs badge-dash cursor-help select-none">
-      <span className="tooltip-content">{children}</span>?
+    <span className="tooltip cursor-help">
+      <span className="tooltip-content">{children}</span>
+      <VscInfo className="text-info inline" />
     </span>
   );
 }
